f279f160de24cebdfc168ba18438e48a46761d0c
sch39 <sukronraezpactoer@gmail.com>
2026-01-21 19:11:16 +0700

    feat: update server configuration and Vite setup for public directory

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,16 @@
-package server
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	 http.Handle("/", http.FileServer(http.Dir("./public")))
+	 
+	 log.Println("Server started on :8080")
+	 err := http.ListenAndServe(":8080", nil)
+	 if err != nil {
+		log.Println("Error starting server:", err)
+	 }
+}
